1c02f161460662c9d578a24a5f9a8d7ecaae6b7c
suprajapaidipala <59077819+suprajapaidipala@users.noreply.github.com>
2020-08-05 23:16:00 +0530

    modified test

--- lib/test.dart
+++ lib/test.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "HEllo",
+          ),
+        ),
+      ),
+    );
+  }
+}
